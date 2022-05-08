53e8a5100d077c07caa5da9f26bdd3656cd8d11d
Senanur Pınarcık <senanurpinarcik@gmail.com>
2022-05-08 21:43:03 +0300

    düzeltme

--- ArabamiSatWeb/Controllers/DashboardController.cs
+++ ArabamiSatWeb/Controllers/DashboardController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ArabamiSatWeb.Models;
 using ArabamiSatWeb.Models.Base;
+using System.Dynamic;
+using Newtonsoft.Json;
 
 namespace ArabamiSatWeb.Controllers
 {
     public class DashboardController : Controller
     {
-        private readonly BaseDbContext _context;  
+        private readonly BaseDbContext _context; 
+         
+        public string GetMarkaModelList(IFormCollection collection)
+        {
+            int markaId = Convert.ToInt32(collection["markaId"]);
+            List<MarkaModel> markaModelList = _context.MarkaModel.Where(i => i.MarkaId == markaId).ToList();
+            List<dynamic> dynamicList = new List<dynamic>();
+            foreach (MarkaModel markaModel in markaModelList)
+            {
+                dynamic foo = new ExpandoObject();
+                foo.Id = markaModel.Id; 
+                foo.Name = markaModel.Ad;
+                dynamicList.Add(foo);
+            }
+             
+            string json = JsonConvert.SerializeObject(dynamicList);
+            return json;
+        }
 
         public async Task<string> UploadImage(IFormFile file)
         {
@@ -47,22 +66,26 @@ namespace ArabamiSatWeb.Controllers
             List<Marka> markaList = _context.Marka.ToList();
             ViewBag.MarkaList = markaList;
 
+            int markaId = Convert.ToInt32(collection["MarkaId"]);
+            int markaModelId = Convert.ToInt32(collection["MarkaModelId"]);
+            int yil = Convert.ToInt32(collection["Yil"]);
+            decimal fiyat = Convert.ToDecimal(collection["Fiyat"]);
+            int durumId = Convert.ToInt32(collection["DurumId"]);
             string aciklama = collection["Aciklama"];
             IFormFile fotograf = collection.Files.First(i => i.Name == "Fotograf");
             string fotografPath = UploadImage(fotograf).Result;
 
 
             Araba araba = new Araba
-            { 
+            {
+                MarkaId = markaId,
+                MarkaModelId = markaModelId,
+                Yil = yil,
+                Fiyat = fiyat,
+                DurumId = durumId,
                 Aciklama = aciklama,
-                Fotograf = fotografPath,
-                Fiyat = 200000,
-                Yil = 2010,
-                DurumId = 0,
-                MarkaId = 0,
-                MarkaModelId = 1,
-                SilindiMi = false
-                
+                Fotograf = fotografPath, 
+                SilindiMi = false                 
             };
 
             _context.Araba.Add(araba);
--- ArabamiSatWeb/Models/Araba.cs
+++ ArabamiSatWeb/Models/Araba.cs
@@ -1,34 +1,44 @@
 ﻿using ArabamiSatWeb.Models.Base;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ArabamiSatWeb.Models
 {
     public class Araba : BaseModel
     {
-        
         public enum DurumEnum
         {
+            [Description("Sıfır")]
             Sifir = 0,
+            [Description("İkinci El")]
             IkinciEl = 2
         }
 
+        [DisplayName("Marka")]
         [ForeignKey("Marka")]
         public int MarkaId { get; set; }
 
+        [DisplayName("Model")]
         [ForeignKey("MarkaModel")]
         public int MarkaModelId { get; set; }
+
+        [DisplayName("Yıl")] 
         public int Yil { get; set; }
 
+        [DisplayName("Fiyat")]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Fiyat { get; set; }
+
+        [DisplayName("Durum")] 
         public int DurumId { get; set; }
 
+        [DisplayName("Açıklama")]
         [Column(TypeName = "Nvarchar(500)")]
         public string Aciklama { get; set; }
 
+        [DisplayName("Fotoğraf")] 
         [Column(TypeName = "Nvarchar(MAX)")]
         public string Fotograf { get; set; }
-
         public virtual Marka Marka { get; set; }
         public virtual MarkaModel MarkaModel { get; set; }
 
