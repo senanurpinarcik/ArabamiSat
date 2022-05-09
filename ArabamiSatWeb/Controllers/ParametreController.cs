372412fe0e516f8c3d441d1d5b2277ec646c4ffa
Senanur Pınarcık <senanurpinarcik@gmail.com>
2022-05-09 20:06:51 +0300

    markamodel

--- ArabamiSatWeb/Controllers/ParametreController.cs
+++ ArabamiSatWeb/Controllers/ParametreController.cs
@@ -88,5 +88,81 @@ namespace ArabamiSatWeb.Controllers
 
             return View(marka);
         }
+
+
+        public IActionResult MarkaModelListe()
+        {
+            List<MarkaModel> MarkaModelListe = _context.MarkaModel.ToList()
+                .Where(i => !i.SilindiMi).ToList();
+            return View(MarkaModelListe);
+        }
+
+        public IActionResult MarkaModelEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MarkaModelEkle(IFormCollection collection)
+        {
+            string ad = collection["Ad"];
+
+            MarkaModel model = new MarkaModel()
+            {
+                Ad = ad
+            };
+
+            _context.MarkaModel.Add(model);
+            int returnValue = _context.SaveChanges();
+
+            if (returnValue > 0)
+                ViewData["SuccessMessage"] = "İşleminiz başarılı bir şekilde gerçekleştirilmiştir.";
+            else
+                ViewData["ErrorMessage"] = "İşleminiz sırasında bir hata oluştu";
+
+            return View(model);
+        }
+
+        public IActionResult MarkaModelGuncelle(int id)
+        {
+            MarkaModel model = _context.MarkaModel.Find(id)!;
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult MarkaModelGuncelle(IFormCollection collection)
+        {
+            int id = Convert.ToInt32(collection["Id"]);
+            string ad = collection["Ad"];
+
+            MarkaModel model  = _context.MarkaModel.Find(id)!;
+            model.Ad = ad;
+
+            _context.MarkaModel.Update(model);
+            int returnValue = _context.SaveChanges();
+
+            if (returnValue > 0)
+                ViewData["SuccessMessage"] = "İşleminiz başarılı bir şekilde gerçekleştirilmiştir.";
+            else
+                ViewData["ErrorMessage"] = "İşleminiz sırasında bir hata oluştu";
+
+            return View(model);
+        }
+
+        public IActionResult MarkaModelSil(int id)
+        {
+            MarkaModel model = _context.MarkaModel.Find(id)!;
+            model.SilindiMi = true;
+
+            _context.MarkaModel.Update(model);
+            int returnValue = _context.SaveChanges();
+
+            if (returnValue > 0)
+                ViewData["SuccessMessage"] = "İşleminiz başarılı bir şekilde gerçekleştirilmiştir.";
+            else
+                ViewData["ErrorMessage"] = "İşleminiz sırasında bir hata oluştu";
+
+            return View(model);
+        }
     }
 }
